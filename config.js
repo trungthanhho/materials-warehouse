db535f8902948b200a5610f4d973592b93b11cec
trungthanhho <hothanhtrung09@gmail.com>
2026-06-09 21:21:10 +0700

    Update warehouse Apps Script URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.WAREHOUSE_CONFIG = {
   // Paste the deployed Apps Script /exec URL here so every device uses the same sync target.
-  sheetUrl: "https://script.google.com/macros/s/AKfycbwaOWd3WKULgk-pKRfE_lwfUz1YTS4GQBSBuxM66dPKqBYSSvtyf-BtRXh7B6mh6cbFFQ/exec",
+  sheetUrl: "https://script.google.com/macros/s/AKfycbwMPiUUA4WwYfKJJmezSSdftmVeKA8ky9x42DbbAgPArB2sVK8A7xIJRsWYn_Oi5HhVug/exec",
   materialsUrl: "materials.json",
   materialsVersion: "2026-06-08-ccdc-1",
   dbName: "MA CCDC.xlsx"
